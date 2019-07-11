ee4733004814181a90f0c4df3166c17f65a94774
Augusto <augustocristian@uniovi.es>
2019-07-11 14:19:40 +0200

    Solving some warnings

--- src/test/java/com/fullteaching/backend/e2e/UserTest.java
+++ src/test/java/com/fullteaching/backend/e2e/UserTest.java
@@ -2,9 +2,6 @@ package com.fullteaching.backend.e2e;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
-import java.io.IOException;
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -30,103 +27,103 @@ import io.github.bonigarcia.wdm.FirefoxDriverManager;
 @ExtendWith(SeleniumExtension.class)
 public class UserTest extends FullTeachingTestE2E {
 
-	 private static String TEACHER_BROWSER;
-	    private static String STUDENT_BROWSER;
+	private static String TEACHER_BROWSER;
+	private static String STUDENT_BROWSER;
 
-	    static Exception ex = null;
+	static Exception ex = null;
 
-	    final String teacherMail = "teacher@gmail.com";
-	    final String teacherPass = "pass";
-	    final String teacherName = "Teacher Cheater";
-	    final String studentMail = "student1@gmail.com";
-	    final String studentPass = "pass";
-	    final String studentName = "Student Imprudent";
+	final String teacherMail = "teacher@gmail.com";
+	final String teacherPass = "pass";
+	final String teacherName = "Teacher Cheater";
+	final String studentMail = "student1@gmail.com";
+	final String studentPass = "pass";
+	final String studentName = "Student Imprudent";
 
-	    BrowserUser user;
+	BrowserUser user;
 
-	
 
-	    @BeforeAll()
-	    static void setupAll() {
 
-	        if (System.getenv("ET_EUS_API") == null) {
-	            // Outside ElasTest
-	            ChromeDriverManager.getInstance().setup();
-	            FirefoxDriverManager.getInstance().setup();
-	        }
+	@BeforeAll()
+	static void setupAll() {
 
-	        if (System.getenv("ET_SUT_HOST") != null) {
-	            APP_URL = "https://" + System.getenv("ET_SUT_HOST") + ":5000/";
-	        } else {
-	            APP_URL = System.getProperty("app.url");
-	            if (APP_URL == null) {
-	                APP_URL = "https://localhost:5000/";
-	            }
-	        }
+		if (System.getenv("ET_EUS_API") == null) {
+			// Outside ElasTest
+			ChromeDriverManager.getInstance().setup();
+			FirefoxDriverManager.getInstance().setup();
+		}
 
-	        TEACHER_BROWSER = System.getenv("TEACHER_BROWSER");
-	        STUDENT_BROWSER = System.getenv("STUDENT_BROWSER");
+		if (System.getenv("ET_SUT_HOST") != null) {
+			APP_URL = "https://" + System.getenv("ET_SUT_HOST") + ":5000/";
+		} else {
+			APP_URL = System.getProperty("app.url");
+			if (APP_URL == null) {
+				APP_URL = "https://localhost:5000/";
+			}
+		}
 
-	        if ((TEACHER_BROWSER == null) || (!TEACHER_BROWSER.equals(FIREFOX))) {
-	            TEACHER_BROWSER = CHROME;
-	        }
+		TEACHER_BROWSER = System.getenv("TEACHER_BROWSER");
+		STUDENT_BROWSER = System.getenv("STUDENT_BROWSER");
 
-	        if ((STUDENT_BROWSER == null) || (!STUDENT_BROWSER.equals(FIREFOX))) {
-	            STUDENT_BROWSER = CHROME;
-	        }
+		if ((TEACHER_BROWSER == null) || (!TEACHER_BROWSER.equals(FIREFOX))) {
+			TEACHER_BROWSER = CHROME;
+		}
 
-	        log.info("Using URL {} to connect to openvidu-testapp", APP_URL);
-	    }
+		if ((STUDENT_BROWSER == null) || (!STUDENT_BROWSER.equals(FIREFOX))) {
+			STUDENT_BROWSER = CHROME;
+		}
 
-	    @AfterEach
-	    void dispose(TestInfo info) {
-	        try {
-	            this.logout(user);
-	            user.dispose();
-	        } finally {
-	            log.info("##### Finish test: "
-	                    + info.getTestMethod().get().getName());
-	        }
-	    }
+		log.info("Using URL {} to connect to openvidu-testapp", APP_URL);
+	}
 
-	    @Test
+	@AfterEach
+	void dispose(TestInfo info) {
+		try {
+			this.logout(user);
+			user.dispose();
+		} finally {
+			log.info("##### Finish test: "
+					+ info.getTestMethod().get().getName());
+		}
+	}
+
+	@Test
 	public void loginTest() throws ElementNotFoundException, BadUserException, NotLoggedException, TimeOutExeception {
 
 		user= setupBrowser("chrome",teacherName,teacherMail,100);
 		WebDriver driver=user.getDriver();
 		try {
 			this.slowLogin(user, teacherMail, teacherPass);
-		
+
 			driver = UserUtilities.checkLogin(driver, teacherMail);
 
 			assertTrue(true, "not logged");
 
 		} catch (NotLoggedException | BadUserException e) {
-				
+
 			e.printStackTrace();
 			fail("Not logged");
-			
+
 		} catch (ElementNotFoundException e) {
-			
+
 			e.printStackTrace();
 			fail(e.getLocalizedMessage());
-			
+
 		} 
-		
+
 		try {
 			driver = UserUtilities.logOut(driver,APP_URL);
-			
+
 			driver = UserUtilities.checkLogOut(driver);
-			
+
 		} catch (ElementNotFoundException enfe) {
 			fail("Still logged");
-			
+
 		} catch (NotLoggedException e) {
 			assertTrue(true, "Not logged");
 		}
-			
+
 		assertTrue(true);
 	}
-	
-	
+
+
 }
